c2665fbb162555ce365632b0cede9c951c0f6846
victor pugin <victor.pugin@epitech.eu>
2018-04-25 06:52:07 +0200

    clean useless drawables

--- app/src/main/java/com/bjtutravel/bjtutravelagency/MainActivity.java
+++ app/src/main/java/com/bjtutravel/bjtutravelagency/MainActivity.java
@@ -67,6 +67,7 @@ public class MainActivity extends AppCompatActivity
 
     private void onCreateAdmin() {
         mUserIsAdmin = true;
+        setTitle(getTitle() + " - Staff");
 
         // FLOATING BUTTON
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
