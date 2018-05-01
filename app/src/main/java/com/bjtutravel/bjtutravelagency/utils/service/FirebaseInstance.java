936ac4e3e6cb69c2e0c6ec59a5c785e7a66d5141
victor pugin <victor.pugin@epitech.eu>
2018-05-01 10:32:55 +0200

    add FirebaseInstanceId service

---
+++ app/src/main/java/com/bjtutravel/bjtutravelagency/utils/service/FirebaseInstance.java
@@ -0,0 +1,35 @@
+package com.bjtutravel.bjtutravelagency.utils.service;
+
+import android.util.Log;
+
+import com.bjtutravel.bjtutravelagency.utils.UtilFirebase;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class FirebaseInstance extends FirebaseInstanceIdService {
+    private static final String TAG = "FirebaseInstance";
+
+    @Override
+    public void onTokenRefresh() {
+        super.onTokenRefresh();
+
+        String currentToken = FirebaseInstanceId.getInstance().getToken();
+        FirebaseUser firebaseUser = UtilFirebase.getFirebaseUser();
+        Log.d(TAG, "New firebase token");
+
+        if (firebaseUser != null) {
+            DatabaseReference ref = FirebaseDatabase.getInstance().getReference()
+                    .child("users")
+                    .child(firebaseUser.getUid());
+
+            if (currentToken != null)
+                ref.child("fcmTokens").child(currentToken).setValue(true);
+            else
+                Log.i(TAG, "onTokenRefresh: token was null.");
+            Log.d(TAG, "New firebase token : " + currentToken);
+        }
+    }
+}
